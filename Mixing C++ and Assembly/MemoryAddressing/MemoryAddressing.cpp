6d980dc42e103f6e91aabbcfd2f0a9b504269d23
rebeccca-debug <54553333+rebeccca-debug@users.noreply.github.com>
2022-01-06 14:46:59 -0500

    Add files via upload

---
+++ Mixing C++ and Assembly/MemoryAddressing/MemoryAddressing.cpp
@@ -0,0 +1,29 @@
+// MemoryAddressing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+
+extern "C" int MemoryAddressing(int i, int* v1, int *v2, int * v3, int *v4);
+extern "C" int NumFibVals;
+
+
+int main()
+{
+	int v1 = 0, v2 = 0, v3 = 0, v4 = 0;
+	int rv;
+
+	for (int i = 0; i <= NumFibVals; i++)
+	{
+		rv = MemoryAddressing(i, &v1, &v2, &v3, &v4);
+
+		printf("rv:  %5d   v1:   %5d    v2:   %5d   v3:   %5d    v4:    %5d \n", rv, v1, v2, v3, v4);
+
+	}
+
+	return 0;
+
+
+}
+
